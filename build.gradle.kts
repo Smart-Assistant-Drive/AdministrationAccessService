7dc8ff5ac9b181447e9d3ca37aab14839a31ad20
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2024-09-28 00:13:15 +0200

    chore: add git sem ver, dokka and git hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,27 @@
+import io.github.andreabrighi.gradle.gitsemver.conventionalcommit.ConventionalCommit
+
 plugins {
+    alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.kotlin.qa)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.taskTree)
     alias(libs.plugins.kotlin.jvm)
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+gitSemVer {
+    maxVersionLength.set(20)
+    buildMetadataSeparator.set("-")
+    commitNameBasedUpdateStrategy(ConventionalCommit::semanticVersionUpdate)
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.convetional)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -16,6 +34,7 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
