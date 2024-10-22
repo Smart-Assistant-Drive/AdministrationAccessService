cbc03d3339ceaa32d0ff7467aeb9e80232e7695a
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2024-10-22 20:08:54 +0200

    test: in spring boot test add MockBean

--- rest/src/test/kotlin/com/example/rest/RestHateoasApplicationTests.kt
+++ rest/src/test/kotlin/com/example/rest/RestHateoasApplicationTests.kt
@@ -1,11 +1,18 @@
 package com.example.rest
 
+import com.example.rest.businessLayer.boundaries.UserInputBoundary
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
 class RestHateoasApplicationTests {
+    @MockBean
+    private var userInputBoundary: UserInputBoundary? = null
+
     @Test
     fun contextLoads() {
+        userInputBoundary = mock<UserInputBoundary>()
     }
 }
